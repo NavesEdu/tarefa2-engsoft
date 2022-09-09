dae0bd32f752dff4ab70585322002eeda64180a7
Your Name <you@example.com>
2022-09-09 14:32:10 -0300

    nova funcao

---
+++ flutter_application_1/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\55359\\flutter\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\55359\\Desktop\\flutter projects\\flutter_application_1" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\55359\\flutter\\flutter"
+  "PROJECT_DIR=C:\\Users\\55359\\Desktop\\flutter projects\\flutter_application_1"
+  "FLUTTER_ROOT=C:\\Users\\55359\\flutter\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\55359\\Desktop\\flutter projects\\flutter_application_1\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\55359\\Desktop\\flutter projects\\flutter_application_1"
+  "FLUTTER_TARGET=C:\\Users\\55359\\Desktop\\flutter projects\\flutter_application_1\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\55359\\Desktop\\flutter projects\\flutter_application_1\\.dart_tool\\package_config.json"
+)
